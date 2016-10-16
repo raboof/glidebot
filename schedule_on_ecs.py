ac8f384cf87550fbc86148b9fe2cb66f79c7fe17
Arnout Engelen <arnouten@bzzt.net>
2016-10-16 12:49:09 +0200

    Script to schedule run on ECS

---
+++ schedule_on_ecs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import boto3
+
+if len(sys.argv) != 2:
+  print("Usage:   %s [repo]" % sys.argv[0]);
+  print("Example: %s github.com/raboof/connbeat" % sys.argv[0]);
+  exit(-1)
+
+owner = sys.argv[1].split('/')[1]
+project = sys.argv[1].split('/')[2]
+
+client = boto3.client('ecs')
+response = client.run_task(
+  taskDefinition='glidebot:3',
+  overrides={ 
+    'containerOverrides': [ 
+      { 
+        'name': 'glidebot',
+        'environment': [ 
+          { 
+            'name': 'USER',
+            'value': 'root'
+          },
+          { 
+            'name': 'OWNER',
+            'value': owner
+          },
+          { 
+            'name': 'PROJECT',
+            'value': project
+          }
+        ]
+      }
+    ]
+  }
+)
+print(response);
